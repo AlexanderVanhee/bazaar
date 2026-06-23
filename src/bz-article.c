0d08f10f1c0682ab24946eb33d02ad1e5e4a82e6
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2026-06-23 18:40:25 +0200

    Normalize padding for images in articles

--- src/bz-article.c
+++ src/bz-article.c
@@ -412,6 +412,7 @@ markdown_bind_inline_uri (BzArticle         *self,
 
       button = gtk_button_new ();
       gtk_widget_add_css_class (button, "flat");
+      gtk_widget_add_css_class (button, "article-image-button");
       gtk_widget_set_hexpand (button, TRUE);
       gtk_widget_set_halign (button, GTK_ALIGN_FILL);
       gtk_button_set_child (GTK_BUTTON (button), picture);
